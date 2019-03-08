c84dd1479fe41969650852945e79434f950dab30
tatsuya <ryuuma1005@gmail.com>
2019-03-08 23:54:00 +0900

    first commit

---
+++ components/layout.js
@@ -0,0 +1,45 @@
+import { css } from 'glamor'
+import Head from 'next/head'
+
+/**
+ * 全ページ共通のレイアウト
+ *
+ * titleやmeta等、HTMLのhead要素にあたる情報は
+ * Headコンポーネント内に書く
+ */
+export default (props) => (
+  <div>
+    <Head>
+      <title>{ props.title || '⚡ハロー、React⚡' }</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      <style jsx global>{`
+       body { margin: 0; }
+      `}</style>
+    </Head>
+
+    <div className={wrapperStyle}>
+      <header className={headerStyle}>
+        <h1>⚡ハロー、React⚡</h1>
+      </header>
+      <div className={contentStyle}>
+        { props.children }
+      </div>
+    </div>
+  </div>
+)
+
+const wrapperStyle = css({
+  maxWidth: 960,
+  margin: '0 auto',
+  textAlign: 'center',
+})
+
+const headerStyle = css({
+  margin: '60px 0 40px',
+  fontStyle: 'italic',
+})
+
+const contentStyle = css({
+  color: 'dimgray',
+  margin: 10,
+})
--- pages/about.js
+++ pages/about.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
  * ReactElementを返す関数になっている
  */
 export default () => (
-  <Layout title="Hello">
-    <h2>Hello world!!!!</h2>
+  <Layout title="About me">
+    <h2>About me</h2>
 
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus erat at tristique iaculis. Curabitur vitae erat finibus, sodales nulla vitae, consequat mauris. Fusce fermentum venenatis sem, ac maximus lacus rutrum eget. Cras fermentum mollis odio sit amet iaculis. Phasellus condimentum faucibus lorem sed rutrum. Donec pulvinar et tellus id venenatis. Nullam vel odio vitae massa condimentum ullamcorper at vitae enim. Aenean facilisis arcu nec felis tincidunt, ornare rutrum lectus tincidunt. Sed purus urna, dapibus eu auctor a, lacinia eget mauris. Donec magna diam, egestas varius facilisis vel, commodo in leo.
